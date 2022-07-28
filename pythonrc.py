45d7a66ed5c6ec324703453bfbdb9ce19f5cec88
lonetwin <steve@lonetwin.net>
2022-07-28 18:59:40 +0100

    Start using `isort --profile black pythonrc.py`

--- pythonrc.py
+++ pythonrc.py
@@ -74,9 +74,8 @@ import shlex
 import signal
 import subprocess
 import webbrowser
-
 from code import InteractiveConsole
-from functools import partial, lru_cache, cached_property
+from functools import cached_property, lru_cache, partial
 from itertools import chain
 from operator import attrgetter
 from tempfile import NamedTemporaryFile
